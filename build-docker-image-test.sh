c5a79c2cf910056390003905956439bb8ae6f751
Javier Martin <javiermg1986@gmail.com>
2024-03-30 17:15:46 +0100

    added git pull command to build scripts

--- build-docker-image-test.sh
+++ build-docker-image-test.sh
@@ -1,4 +1,5 @@
 git fetch
+git pull
 git checkout develop
 docker rm aimharderbot_test:v1
 docker rmi aimharderbot_test:v1
--- build-docker-image.sh
+++ build-docker-image.sh
@@ -1,4 +1,5 @@
 git fetch
+git pull
 git checkout master
 docker rm aimharderbot:v1
 docker rmi aimharderbot:v1
