7573d22e740aeefc2ebbc9044fa8247cb0a2ea8d
pachiclana <javiermg1986@gmail.com>
2024-03-30 17:26:42 +0100

    added git pull command to build scripts

--- build-docker-image-test.sh
+++ build-docker-image-test.sh
@@ -1,6 +1,5 @@
-git fetch
-git pull
 git checkout develop
+git pull
 docker rm aimharderbot_test:v1
 docker rmi aimharderbot_test:v1
 docker build -t aimharderbot_test:v1 .
--- build-docker-image.sh
+++ build-docker-image.sh
@@ -1,6 +1,5 @@
-git fetch
-git pull
 git checkout master
+git pull
 docker rm aimharderbot:v1
 docker rmi aimharderbot:v1
 docker build -t aimharderbot:v1 .
