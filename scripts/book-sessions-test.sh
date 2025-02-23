8503c511dbcdd5a69d22d466c52ae6c3ee6e92cb
pachiclana <javiermg1986@gmail.com>
2025-02-23 18:26:08 +0100

    deleted unused script

--- scripts/book-sessions-test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /home/pi/aimharderbot
-
-#We run the docker container and book the session according to the parameters
-docker run --rm -v $(pwd)/logs/test:/usr/src/app/logs \
-				-v $(pwd)/config:/usr/src/app/config \
-    			--name aimharderbot-test aimharderbot_test:v1 \
-				--config-filename='aimharderbot_config.yaml'
